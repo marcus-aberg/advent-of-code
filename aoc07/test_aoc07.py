49391dcd1f7399a7d66dbe80bdcd2d47ce54e300
maraberg <marcus.aberg@modelon.com>
2022-12-07 20:03:25 +0100

    adding day 7

---
+++ aoc07/test_aoc07.py
@@ -0,0 +1,56 @@
+# test_aoc_template.py
+
+import pathlib
+import pytest
+import aoc07 as aoc
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == ['$ cd /',
+                        '$ ls',
+                        'dir a',
+                        '14848514 b.txt',
+                        '8504156 c.dat',
+                        'dir d',
+                        '$ cd a',
+                        '$ ls',
+                        'dir e',
+                        '29116 f',
+                        '2557 g',
+                        '62596 h.lst',
+                        '$ cd e',
+                        '$ ls',
+                        '584 i',
+                        '$ cd ..',
+                        '$ cd ..',
+                        '$ cd d',
+                        '$ ls',
+                        '4060174 j',
+                        '8033020 d.log',
+                        '5626152 d.ext',
+                        '7214296 k']
+
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc.part1(example1) == 95437
+
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc.part2(example1) == 24933642
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc.part2(example2) == ...
