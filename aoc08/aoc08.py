d7e92d5dc95a76142b8b926ee9eebd2ed27c9376
maraberg <marcus.aberg@modelon.com>
2022-12-08 17:33:06 +0100

    Adding day 8

---
+++ aoc08/aoc08.py
@@ -0,0 +1,71 @@
+# aoc_template.py
+
+import pathlib
+import sys
+
+import numpy
+
+def parse(puzzle_input):
+    """Parse input."""
+    rows = puzzle_input.split("\n")
+    rows = [list(map(int,r)) for r in rows]
+    return numpy.array(rows)
+
+def is_visible(idx, x,data):
+    i, j = (idx[0] + 1,idx[1] + 1)
+    v_l = all(x > data[i,:j])
+    v_r = all(x > data[i,j+1:])
+    v_a = all(x > data[:i,j])
+    v_b = all(x > data[i+1:,j])
+    if any([v_l,v_r,v_a,v_b]):
+        return True
+    
+def part1(data):
+    """Solve part 1."""
+    shape = data.shape
+    visible = 2*shape[0]+2*shape[1]-4
+    for idx, x in numpy.ndenumerate(data[1:-1, 1:-1]):
+        if is_visible(idx, x, data):
+            visible += 1
+    
+    return visible
+
+def get_view_score(x, trees_in_direction):
+    visible_trees = numpy.where(x <= trees_in_direction)[0]+1 
+    if len(visible_trees) == 0:
+        visible_trees = len(trees_in_direction)
+    else:
+        visible_trees = visible_trees[0]
+    
+    return visible_trees
+    
+def part2(data):
+    """Solve part 2."""
+    max_view_score = 0
+    for idx, x in numpy.ndenumerate(data[1:-1, 1:-1]):
+        if is_visible(idx, x, data):
+            i, j = (idx[0] + 1,idx[1] + 1)
+            d_l = get_view_score(x, numpy.flip(data[i,:j]))
+            d_r = get_view_score(x, data[i,j+1:])
+            d_a = get_view_score(x, numpy.flip(data[:i,j]))
+            d_b = get_view_score(x, data[i+1:,j])
+            cur_view_score = d_l*d_r*d_a*d_b
+            if cur_view_score > max_view_score:
+                max_view_score = cur_view_score
+    
+    return max_view_score
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip() # TODO: For some problems strip() is inefficient, spaces might be significant
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
---
+++ aoc08/test_aoc08.py
@@ -0,0 +1,43 @@
+# test_aoc_template.py
+
+import pathlib
+import pytest
+import aoc08 as aoc
+import numpy
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+# TODO: Some problems have more than one example
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    comparison = example1 == numpy.array([[3,0,3,7,3],
+                        [2,5,5,1,2],
+                        [6,5,3,3,2],
+                        [3,3,5,4,9],
+                        [3,5,3,9,0]])
+    
+    assert comparison.all()
+    
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc.part1(example1) == 21
+
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc.part2(example1) == 8
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc.part2(example2) == ...
